d60dec4ef309b019d982ef91ea3d08ebc272f112
Thugmek <tomekmichalhu@seznam.cz>
2025-01-19 21:46:01 +0100

    some work on controll app

---
+++ ControllApp/MainWindow.py
@@ -0,0 +1,32 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="GammaScintillator")
+        self.connect("destroy", Gtk.main_quit)
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        #self.add(self.button)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label(label="Default Page!"))
+        self.notebook.append_page(self.page1, Gtk.Label(label="Plain Title"))
+
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
+        self.notebook.append_page(
+            self.page2, Gtk.Image.new_from_icon_name("help-about", Gtk.IconSize.MENU)
+        )
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
---
+++ ControllApp/main.py
@@ -0,0 +1,11 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+from MainWindow import MainWindow
+
+mw = MainWindow()
+mw.show_all()
+
+Gtk.main()
